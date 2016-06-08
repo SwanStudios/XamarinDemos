bb024d609e57c40efe6025f0835aff1e702d4f31
Anil Kumar Sutrala <anilkumarsutrala@gmail.com>
2016-06-08 22:57:32 +0530

    Checkin in a Demo Project

---
+++ PhoneWord/PhoneWord/PhoneWord/MainPage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using Xamarin.Forms;
+
+public class MainPage : ContentPage
+{
+    Entry lEntry;
+    Button lTranslateButton;
+    Button lCallButton;
+
+    public MainPage()
+    {
+        this.Padding = new Thickness(20,
+            Device.OnPlatform<double>(40, 20, 20), 20, 20);
+
+        var lLabel = new Label
+        {
+            HorizontalTextAlignment = TextAlignment.Start,
+            Text = "Enter the phone number"
+        };
+
+        lEntry = new Entry
+        {
+            HorizontalTextAlignment = TextAlignment.Start,
+            Text = "91-9885822244"
+        };
+
+        lTranslateButton = new Button
+        {
+            Text = "Translate"
+        };
+
+        lCallButton = new Button
+        {
+            Text = "Call",
+            IsEnabled = false
+
+        };
+
+        var mainStackLayout = new StackLayout
+        {
+            VerticalOptions = LayoutOptions.FillAndExpand,
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+            Orientation = StackOrientation.Vertical,
+            Spacing = 15,
+            Children = {
+                lLabel,
+                lEntry,
+                lTranslateButton,
+                lCallButton
+            }
+        };
+
+        this.Content = mainStackLayout;
+
+        lTranslateButton.Clicked += LTranslateButton_Clicked;
+    }
+
+    private void LTranslateButton_Clicked(object sender, EventArgs e)
+    {
+        string lInputString = lEntry.Text;
+        string translatedNumber = Core.PhoneNumberTranslator.ToNumber(lInputString);
+
+        if(!String.IsNullOrEmpty(translatedNumber))
+        {
+            lCallButton.Text = "Call " + translatedNumber;
+            lCallButton.IsEnabled = true;
+        }
+        else
+        {
+            lCallButton.Text = "Call";
+            lCallButton.IsEnabled = false;
+        }
+
+    }
+
+}
+
---
+++ PhoneWord/PhoneWord/PhoneWord/PhoneNumberTranslator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class PhoneNumberTranslator
+    {
+        public static string ToNumber(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return null;
+
+            str = str.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach ( var c in str)
+            {
+                if (" -0123456789".Contains(c))
+                    newNumber.Append(c);
+
+                else
+                {
+                    var result = TranslateToNumber(c);
+                    if (result != null)
+                        newNumber.Append(result);
+                    // Bad character?
+                    else
+                        return null;
+                }
+            }
+            return newNumber.ToString();
+        }
+
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        static readonly string[] digits = {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                    return 2 + i;
+            }
+            return null;
+        }
+    }
+}
